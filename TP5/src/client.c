229d33a850dbe4d09c19a3bb8626f9e7ca77ba8a
John Samuel <johnsamuelwrites@gmail.com>
2020-09-13 13:44:35 +0200

    Update execise code

---
+++ TP5/src/client.c
@@ -0,0 +1,77 @@
+/*
+ * SPDX-FileCopyrightText: 2020 John Samuel
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ */
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>  
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include "client.h"
+
+/* envoi et reception de message
+ */
+int envoie_recois_message(int socketfd) {
+  char data[1024];
+  memset(data, 0, sizeof(data));
+  char message[100];
+  printf("Votre message (max 1000 caracteres): ");
+  fgets(message, 1024, stdin);
+  strcpy(data, "message: ");
+  strcat(data, message);
+  
+  int write_status = write(socketfd, data, strlen(data));
+  if ( write_status < 0 ) {
+    perror("erreur ecriture");
+    exit(EXIT_FAILURE);
+  }
+
+  memset(data, 0, sizeof(data));
+  int read_status = read(socketfd, data, sizeof(data));
+  if ( read_status < 0 ) {
+    perror("erreur lecture");
+    return -1;
+  }
+
+  printf("Message recu: %s\n", data);
+ 
+  return 0;
+}
+
+int main() {
+  int socketfd;
+  int bind_status;
+
+  struct sockaddr_in server_addr, client_addr;
+
+  /*
+   * Creation d'un socket
+   */
+  socketfd = socket(AF_INET, SOCK_STREAM, 0);
+  if ( socketfd < 0 ) {
+    perror("socket");
+    exit(EXIT_FAILURE);
+  }
+
+  //détails du serveur (adresse et port)
+  memset(&server_addr, 0, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(PORT);
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+
+  //demande de connection au serveur
+  int connect_status = connect(socketfd, (struct sockaddr *) &server_addr, sizeof(server_addr));
+  if ( connect_status < 0 ) {
+    perror("connection serveur");
+    exit(EXIT_FAILURE);
+  }
+  envoie_recois_message(socketfd);
+
+  close(socketfd);
+}
---
+++ TP5/src/serveur.h
@@ -0,0 +1,20 @@
+/*
+ * SPDX-FileCopyrightText: 2020 John Samuel
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ */
+
+
+#ifndef __SERVER_H__
+#define __SERVER_H__
+
+#define PORT 8089
+
+/* accepter la nouvelle connection d'un client et lire les données
+ * envoyées par le client. En suite, le serveur envoie un message
+ * en retour
+ */
+int recois_envoie_message(int socketfd);
+
+#endif
